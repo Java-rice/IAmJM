0358f298400b1017be1ed6e6edde6c68f0a85110
Java-rice <johnmark.p.peroche@gmail.com>
2024-07-21 22:21:43 +0800

    added home page and initialize necessary setup for tailwind

--- IAmJm/src/App.jsx
+++ IAmJm/src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Name from "./components/logo/Name";
+import Home from "./pages/home/Home";
+import TransitionWrapper from "./components/transition/TransitionWrapper";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="bg-[#3B4046] min-h-screen min-w-screen">
+      <Name></Name>
+      <Router>
+        <TransitionWrapper>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </TransitionWrapper>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
---
+++ IAmJm/src/components/cards/AnimatedProfile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import profile from "../../assets/profile.png";
+
+const AnimatedProfile = () => {
+  return (
+    <div className="w-[55%]">
+        <div></div>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <img src={profile}></img>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default AnimatedProfile
