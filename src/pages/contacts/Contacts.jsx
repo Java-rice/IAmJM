ea2296c5c73f530d4311bc25330331b598e84df9
Java-rice <johnmark.p.peroche@gmail.com>
2024-07-23 10:32:43 +0800

    peroche_branch

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Navigation from "../../components/navigation/Navigation";
 
-const Projects = () => {
+const Contacts = () => {
   return (
     <div className="px-[10%] gap-10 text-[#FDFDFD] font-rubik flex flex-col justify-center items-center h-auto py-[3%]">
-      <div className="w-[100%] text-left">
-        <h1 className="font-bold text-4xl">
-          WORKS <span className="text-[#F5B301]">&</span> PROJECTS
-        </h1>
+      <div className="w-[100%] text-center">
+        <h1 className="font-bold text-4xl">CONTACT ME</h1>
       </div>
-      <div></div>
       <Navigation></Navigation>
     </div>
   );
 };
 
-export default Projects;
+export default Contacts;
