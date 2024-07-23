485084fd6e73f930d146eb8e66a9b05f1c421ce1
Java-rice <johnmark.p.peroche@gmail.com>
2024-07-24 05:50:46 +0800

    minor changes

--- src/components/cards/Socials.jsx
+++ src/components/cards/Socials.jsx
@@ -8,10 +8,10 @@ const Socials = () => {
   return (
     <div className="border-b-2 border-t-2 border-[#F5B301] py-[3%]">
       <li className="list-none flex flex-row align-center justify-around">
-        <ul><a className="cursor-pointer w-12 h-12 m-auto"><img src={Instagram}/></a></ul>
-        <ul><a className="cursor-pointer w-12 h-12 m-auto"><img src={Facebook}/></a></ul>
-        <ul><a className="cursor-pointer w-12 h-12 m-auto"><img src={Github}/></a></ul>
-        <ul><a className="cursor-pointer w-12 h-12 m-auto"><img src={LinkedIn}/></a></ul>
+        <ul><a className="cursor-pointer w-12 h-12 m-auto" href="https://www.instagram.com/jamaaaaaaaaaaaak/"><img src={Instagram} className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 duration-300'/></a></ul>
+        <ul><a className="cursor-pointer w-12 h-12 m-auto" href="https://www.facebook.com/johnmark.pacaldoperoche/"><img src={Facebook} className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 duration-300'/></a></ul>
+        <ul><a className="cursor-pointer w-12 h-12 m-auto" href="https://github.com/Java-rice"><img src={Github} className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 duration-300'/></a></ul>
+        <ul><a className="cursor-pointer w-12 h-12 m-auto" href="https://www.linkedin.com/in/john-mark-peroche-61a756229/"><img src={LinkedIn} className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 duration-300'/></a></ul>
       </li>
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ import Navigation from "../../components/navigation/Navigation";
 
 const Home = () => {
   return (
-    <div className="px-[10%] gap-[10%] font-rubik flex lg:flex-row justify-center items-center flex-col-reverse h-auto lg:min-h-screen py-auto">
+    <div className="px-[10%] py-28 gap-20 font-rubik flex lg:flex-row justify-center items-center flex-col-reverse h-auto lg:min-h-screen py-auto">
       <div className="lg:w-[35%] w-[100%] mt-20 lg:text-left  justify-center flex flex-col gap-4 text-center">
         <h2 className="text-5xl text-[#F5B301]">Hello, Its me</h2>
         <h1 className="font-extrabold text-6xl text-[rgb(253,253,253)]">John Mark</h1>
@@ -21,7 +21,7 @@ const Home = () => {
           learning new technologies and willing to contribute on projects.
         </p>
         <Socials></Socials>
-        <div className="flex flex-row gap-10 align-center py-20">
+        <div className="flex flex-row gap-10 align-center m-auto py-20">
           <Button variant="solid">About Me</Button>
           <Button variant="bordered">Contact Me</Button>
         </div>
