826040e4b50a23d95a54d08072af4db77a7d9af9
Java-rice <johnmark.p.peroche@gmail.com>
2024-08-23 13:55:51 +0800

    style: fix the style of navigation bar and links

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import AHome from "../../assets/AHome.png";
 import AAbout from "../../assets/AAbout.png";
@@ -13,36 +13,66 @@ import Blog from "../../assets/Blog.png";
 
 const Navigation = () => {
   return (
-    <div className="fixed top-48 right-20 py-8 border border-[#F5B301]">
-      <nav className="flex flex-row relative right-8 lg:flex-col gap-10 z-10 justify-around">
+    <div className="sticky rounded-md my-12 max-w-[80%] w-full bottom-12 left-0 right-0 lg:my-0 lg:fixed lg:w-auto lg:max-w-none lg:bottom-auto lg:right-32 lg:left-auto lg:top-48 mx-auto lg:py-8 bg-[#3B4046] z-20 pointer-events-none">
+      <nav className="flex flex-row relative lg:flex-col gap-10 justify-around pointer-events-auto">
         <NavLink to="/IAmJM/" end>
           {({ isActive }) => (
-            <img src={isActive ? AHome : Home} className='w-14 h-14' alt="Home" />
+            <div className={`w-14 h-14 flex items-center  p-auto justify-center bg-[#1E2329] relative lg:bottom-0 bottom-4 rounded-full transition-transform transform ${isActive ? "scale-125" : "hover:scale-125"} hover:brightness-110`}>
+              <img
+                src={isActive ? AHome : Home}
+                className={`w-10 h-10 rounded-full transition-transform transform ${isActive ? "scale-115" : "hover:scale-115"}`}
+                alt="Projects"
+              />
+            </div>
           )}
         </NavLink>
         <NavLink to="/IAmJM/about">
           {({ isActive }) => (
-            <img src={isActive ? AAbout : About} className='w-14 h-14' alt="About" />
+            <div className={`w-14 h-14 flex items-center p-auto justify-center bg-[#1E2329] relative lg:bottom-0 bottom-4 rounded-full transition-transform transform ${isActive ? "scale-125" : "hover:scale-125"} hover:brightness-110`}>
+              <img
+                src={isActive ? AAbout : About}
+                className={`w-10 h-10 rounded-full transition-transform transform ${isActive ? "scale-115" : "hover:scale-115"}`}
+                alt="Projects"
+              />
+            </div>
           )}
         </NavLink>
         <NavLink to="/IAmJM/projects">
-          {({ isActive }) => (
-            <img src={isActive ? AProjects : Project} className='w-14 h-14' alt="Projects" />
-          )}
+        {({ isActive }) => (
+          <div className={`w-14 h-14 flex items-center  p-auto justify-center bg-[#1E2329] relative lg:bottom-0 bottom-4 rounded-full transition-transform transform ${isActive ? "scale-125" : "hover:scale-125"} hover:brightness-110`}>
+            <img
+              src={isActive ? AProjects : Project}
+              className={`w-10 h-10 rounded-full transition-transform transform ${isActive ? "scale-115" : "hover:scale-115"}`}
+              alt="Projects"
+            />
+          </div>
+        )}
         </NavLink>
         <NavLink to="/IAmJM/contacts">
           {({ isActive }) => (
-            <img src={isActive ? AContact : Contacts} className='w-14 h-14' alt="Contacts" />
+            <div className={`w-14 h-14 flex items-center  p-auto justify-center bg-[#1E2329] relative lg:bottom-0 bottom-4 rounded-full transition-transform transform ${isActive ? "scale-125" : "hover:scale-125"} hover:brightness-110`}>
+            <img
+              src={isActive ? AContact : Contacts}
+              className={`w-10 h-10 rounded-full transition-transform transform ${isActive ? "scale-115" : "hover:scale-115"}`}
+              alt="Projects"
+            />
+            </div>
           )}
         </NavLink>
         <NavLink to="/IAmJM/blogs">
           {({ isActive }) => (
-            <img src={isActive ? ABlog : Blog} className='w-14 h-14' alt="Blogs" />
+            <div className={`w-14 h-14 flex items-center  p-auto justify-center bg-[#1E2329] relative lg:bottom-0 bottom-4 rounded-full transition-transform transform ${isActive ? "scale-125" : "hover:scale-125"} hover:brightness-110`}>
+            <img
+              src={isActive ? ABlog : Blog}
+              className={`w-10 h-10 rounded-full transition-transform transform ${isActive ? "scale-115" : "hover:scale-115"}`}
+              alt="Projects"
+            />
+            </div>
           )}
         </NavLink>
       </nav>
     </div>
   );
-}
+};
 
 export default Navigation;
