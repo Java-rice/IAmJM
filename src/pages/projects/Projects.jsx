5fb89160951d4b34c38627a78c15bb4e917c7181
Java-rice <johnmark.p.peroche@gmail.com>
2024-08-29 22:28:45 +0800

    feat: added draft carousel

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,27 +1,47 @@
 import React from "react";
+import Slider from "react-slick";
 import ProjectCards from "../../components/cards/ProjectCards";
 import LayoutCards from "../../components/cards/LayoutCards";
 
+// Import Slick Carousel CSS
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const Projects = () => {
+  const carouselSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+
+  };
+
   return (
     <div className="px-[10%] lg:gap-10 gap-3 text-[#FDFDFD] font-rubik flex flex-col justify-center items-center h-auto py-[3%]">
-      <div className="flex flex-col justify-center px-auto">
+      <div className="flex flex-col justify-center w-full">
         <div className="w-full text-center mb-4 text-[#FDFDFD]">
           <h1 className="font-bold text-2xl">
             WORKS <span className="text-[#F5B301]">&</span> PROJECTS
           </h1>
         </div>
-        <ProjectCards></ProjectCards>
-        <div></div>
+        <div className="slider-container w-full lg:w-[100%]">
+          <Slider {...carouselSettings}>
+            {[...Array(5)].map((_, index) => (
+              <div key={index} className="slide-item">
+                <ProjectCards />
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
-      <div>
-        <div className="w-ful mb-4 text-center text-[#FDFDFD]">
+      <div className="w-full">
+        <div className="w-full mb-4 text-center text-[#FDFDFD]">
           <h1 className="font-bold text-2xl">
-          DESIGN <span className="text-[#F5B301]">&</span> LAYOUTS
+            DESIGN <span className="text-[#F5B301]">&</span> LAYOUTS
           </h1>
         </div>
-        <LayoutCards></LayoutCards>
-        <div></div>
+        <LayoutCards />
       </div>
     </div>
   );
