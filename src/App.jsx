013a49c1ba23042924a038f8217e285fd8985c00
Java-rice <johnmark.p.peroche@gmail.com>
2025-01-11 23:52:30 +0800

    feat: initial scroll down and scroll up

--- src/App.jsx
+++ src/App.jsx
@@ -6,59 +6,15 @@ import TransitionWrapper from "./components/transition/TransitionWrapper";
 import Navigation from "./components/navigation/Navigation";
 import Footer from "./components/footer/Footer";
 import ScrollToTop from "./components/scroll/ScrollToTop";
+import Content from "./pages/Content";
 
 function App() {
-  const routes = ["/IAmJM/", "/IAmJM/about", "/IAmJM/projects", "/IAmJM/contacts", "/IAmJM/blogs"];
-  
-  const navigate = useNavigate();
-  
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-      // Scroll to the next route when scrolled to the bottom
-      if (scrollTop + clientHeight >= scrollHeight - 500) {
-        const currentPath = window.location.pathname;
-        const currentIndex = routes.findIndex((route) => route === currentPath);
-        const nextIndex = (currentIndex + 1) % routes.length; 
-        navigate(routes[nextIndex]);
-      }
-
-      if (scrollTop + clientHeight >= scrollHeight + 5) {
-        const currentPath = window.location.pathname;
-        const currentIndex = routes.findIndex((route) => route === currentPath);
-        const nextIndex = (currentIndex - 1) % routes.length; 
-        navigate(routes[nextIndex]);
-      }
-
-      // Scroll to the top logic
-      if (scrollTop === 0) {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [navigate]);
-  
 
   return (
     <div className="flex flex-col min-h-screen bg-[#1E2329] no-zoom-out">
       <Name />
-      <div className="flex-grow overflow-hidden flex-1 flex flex-col">
-        <TransitionWrapper>
-          <ScrollToTop />
-          <Outlet />
-        </TransitionWrapper>
-      </div>
-      <Navigation />
-      <Footer className="mt-auto" />
+      <Content/>
+      <Footer className="" />
     </div>
   );
 }
