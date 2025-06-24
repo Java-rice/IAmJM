a7f62fadef1c6fabf730022e2188c6001516f9df
Java-rice <johnmark.p.peroche@gmail.com>
2025-06-24 18:30:11 +0800

    feat: experience

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -5,21 +5,21 @@ const ExperienceData = {
       organization: "The Oscar's Real Estate and Property Management",
       duration: "February 2025 - Present",
       description:
-       "Designed, develop and deployed document generation website."
+        "As a Frontend Web Developer, I designed and developed a responsive web platform for a foreign market, contributing to over $3,000 in early revenue. I led the full design workflow in Figma—from wireframes to polished designs—prioritizing clarity, trust, and usability for mobile users. I built the frontend using React, Tailwind CSS, HTML, and integrated APIs while optimizing for performance and accessibility. My work improved load speed and ensured design consistency across the platform, enhancing user engagement and operational efficiency. Technologies used: React, TypeScript, JavaScript, Tailwind, WordPress, Git, Figma.",
     },
     {
       role: "Frontend Developer (Part-time)",
       organization: "8box Inc.",
       duration: "October 2024 - February 2025",
       description:
-        "Designed and deployed modern websites for small businesses using modern web stacks.",
+        "During my part-time role as a Frontend Web Developer, I led the frontend development of a web-based PC timer system using React, Tailwind CSS, and REST APIs, implementing features like user session tracking and admin/shop controls. I also helped develop a rental platform modeled after Airbnb, working on booking flows, property listings, and user authentication with clean routing and reusable components. Using Figma designs as the foundation, I built responsive and high-performing UI, applied layout and styling logic, and optimized frontend code for performance using modern React libraries and Git. Technologies used: React, TypeScript, Tailwind CSS, Postman, Git, GitHub, Axios, Vite, Figma..",
     },
     {
       role: "Frontend Developer Intern",
       organization: "LexMeet Inc.",
       duration: "July 2024 - September 2024",
       description:
-        "Assigned to work on a team projects to develop the LexMeet website using ReactJS, enhancing both functionality and user experience.",
+        "As a Frontend Web Developer Intern, I helped build internal web pages using React and CSS, focusing on layout structure, basic routing, and reusable visual components to support the company’s legal tech platform. I also engaged in training sessions on Docker to understand containerization concepts and local development workflows. Collaborating closely with fellow interns, I contributed to projects in a team-based environment, gaining practical experience with Git, Gitlab, and development tools like Chrome DevTools and Visual Studio Code. Technologies used: React, CSS, Docker (basic), Git, Gitlab, Figma (for reference), Visual Studio Code, Chrome DevTools.",
     },
   ],
   organizations: [
